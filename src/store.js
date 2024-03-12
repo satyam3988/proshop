fa6be6af00e24a65e1adc37b4d27238024728191
Satyam Pathak <satyampathak@Satyams-MacBook-Pro.local>
2024-03-11 20:43:48 -0700

    connected frontend with databse

---
+++ src/store.js
@@ -0,0 +1,19 @@
+// import { createStore, combineReducers, applyMiddleware } from 'redux';
+// import thunk from 'redux-thunk';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+
+// // Define your initial state here, if you have any
+// const initialState = {};
+
+// // Combine your reducers here
+// const reducer = combineReducers({
+//   // your reducers go here
+// });
+
+// // Enhance applyMiddleware with composeWithDevTools for Redux DevTools and middleware
+// const middlewareEnhancer = composeWithDevTools(applyMiddleware(thunk));
+
+// // Create the store with the reducer, initial state, and enhanced middleware
+// const store = createStore(reducer, initialState, middlewareEnhancer);
+
+// export default store;
