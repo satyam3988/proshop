0d41f88c92184b89aedf52bd84c6988c671705cd
Satyam Pathak <satyampathak@Satyams-MacBook-Pro.local>
2024-03-13 11:26:08 -0700

    Redux added

--- src/store.js
+++ src/store.js
@@ -1,19 +1,27 @@
-// import { createStore, combineReducers, applyMiddleware } from 'redux';
-// import thunk from 'redux-thunk';
-// import { composeWithDevTools } from 'redux-devtools-extension';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+// import { composeWithDevTools } from 'redux-devtools-extension'
 
-// // Define your initial state here, if you have any
-// const initialState = {};
+// Define a sample reducer as an example. Replace or remove it according to your actual app structure.
+const sampleReducer = (state = {}, action) => {
+  switch (action.type) {
+    // Handle some case
+    // case 'ACTION_TYPE':
+    //     return newState;
+    default:
+      return state;
+  }
+};
 
-// // Combine your reducers here
-// const reducer = combineReducers({
-//   // your reducers go here
-// });
+// Use combineReducers to combine different reducers
+const rootReducer = combineReducers({
+  // Add your reducers here. For now, we have just a sampleReducer.
+  sample: sampleReducer,
+  // Other reducers can be added here as needed.
+});
 
-// // Enhance applyMiddleware with composeWithDevTools for Redux DevTools and middleware
-// const middlewareEnhancer = composeWithDevTools(applyMiddleware(thunk));
+const store = configureStore({
+  reducer: rootReducer,
+  // middleware can be customized here if needed.
+});
 
-// // Create the store with the reducer, initial state, and enhanced middleware
-// const store = createStore(reducer, initialState, middlewareEnhancer);
-
-// export default store;
+export default store;
